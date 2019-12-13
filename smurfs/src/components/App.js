a854a31c67ef37e52383ae6870115f368e42b9ad
newbi462 <isthisemailfree@gmail.com>
2019-12-13 13:54:14 -0500

    Fix start page error

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -26,7 +26,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <h1>SMURFS! 2.0 W/ Context instead of Redux</h1>
       <div>Welcome to your state management version of Smurfs!</div>
       <div>Start inside of your `src/index.js` file!</div>
       <div>Have fun!</div>
