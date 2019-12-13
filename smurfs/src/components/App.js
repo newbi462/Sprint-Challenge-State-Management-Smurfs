2930163209d312ca8e6311005a6acf5d3a57a104
newbi462 <isthisemailfree@gmail.com>
2019-12-13 12:08:26 -0500

    API Data in App state > Context

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,8 +1,25 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+
+import axios from "axios";
+
+import { SmurfContext } from "./../contexts/SmurfContext";
 
 
 function App() {
+  const [smurfsApiDate, setSmurfsApiDate] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3333/smurfs`)
+    .then(response => {
+      //console.log(response.data)
+      setSmurfsApiDate(response.data)
+    })
+    .catch( error => {
+      console.log("error", error.message)
+    })
+  },[])
+  console.log(smurfsApiDate);
 
   return (
     <div className="App">
@@ -10,6 +27,9 @@ function App() {
       <div>Welcome to your state management version of Smurfs!</div>
       <div>Start inside of your `src/index.js` file!</div>
       <div>Have fun!</div>
+
+      <SmurfContext.Provider value={{ smurfsApiDate }}>
+      </SmurfContext.Provider>
     </div>
   );
 }
